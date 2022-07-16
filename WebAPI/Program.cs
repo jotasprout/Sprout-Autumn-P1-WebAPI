0b488d10c985e0ad714742abec80ffda691ac8c6
Jay Sprout <jotasprout@gmail.com>
2022-07-16 16:30:30 -0400

    building WebAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Services;
+using CustomExceptions;
+using DataAccess;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers;
+using Microsoft.AspNetCore.Http.Json;
 
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
