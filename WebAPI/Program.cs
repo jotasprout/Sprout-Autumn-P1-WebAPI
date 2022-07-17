201c0ac5c2b65a6e29e4a0702afbda4afb91fed6
Jay Sprout <jotasprout@gmail.com>
2022-07-17 18:35:09 -0400

    basic api working

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,17 +2,20 @@ using Services;
 using CustomExceptions;
 using DataAccess;
 using Models;
-using Microsoft.AspNetCore.Mvc;
-using WebAPI.Controllers;
-using Microsoft.AspNetCore.Http.Json;
+//using Microsoft.AspNetCore.Mvc;
+//using WebAPI.Controllers;
+//using Microsoft.AspNetCore.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+//builder.Services.AddControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
